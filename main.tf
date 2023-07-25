193795e55d87a55b5fcf6d41824cf635d1487950
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-25 20:41:03 +0200

    Create eventhub

--- main.tf
+++ main.tf
@@ -9,11 +9,3 @@ resource "azurerm_eventhub_namespace" "energy_application_eventhub_namespace" {
   resource_group_name = azurerm_resource_group.energy_application_rg.name
   sku                 = "Standard"  # Change to your desired SKU (Standard, Basic, etc.)
 }
-
-resource "azurerm_eventhub" "energy_application_eventhub" {
-  name                = var.eventhub_name
-  namespace_name      = azurerm_eventhub_namespace.energy_application_eventhub_namespace.name
-  resource_group_name = azurerm_resource_group.energy_application_rg.name
-  partition_count     = 2  # Change to your desired number of partitions
-  message_retention   = 0
-}
