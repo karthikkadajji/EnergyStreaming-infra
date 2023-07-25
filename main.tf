27b804b59e98b6a802e2f90d6e55bdab3006f078
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-25 21:34:01 +0200

    Create eventhub

--- main.tf
+++ main.tf
@@ -13,3 +13,12 @@ resource "azurerm_eventhub_namespace" "energy_application_eventhub_ns" {
     delete = "10m"
   }
 }
+
+resource "azurerm_eventhub" "energy_application_eventhub" {
+  name                = var.eventhub_name
+  namespace_name      = azurerm_eventhub_namespace.energy_application_eventhub_ns.name
+  resource_group_name = azurerm_resource_group.energy_application_rg.name
+  partition_count     = 2
+  message_retention   = 7
+  depends_on = [azurerm_eventhub_namespace.energy_application_eventhub_ns]
+}
