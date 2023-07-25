d5a4a2def24d94cad8f6097b450b8f66f7b5561e
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-25 19:18:06 +0200

    destroy

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ provider "azurerm" {
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-#resource "azurerm_resource_group" "rg-aks" {
-#  name     = var.resource_group_name
-#  location = var.location
-#}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "West Europe"
+}
 
