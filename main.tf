41b2d3c459d4b968727ad25584cfa36fd62a4112
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-25 20:41:49 +0200

    Create eventhub

--- main.tf
+++ main.tf
@@ -2,10 +2,3 @@ resource "azurerm_resource_group" "energy_application_rg" {
   name     = var.application_rg
   location = var.location
 }
-
-resource "azurerm_eventhub_namespace" "energy_application_eventhub_namespace" {
-  name                = var.eventhub_namespace_name
-  location            = azurerm_resource_group.energy_application_rg.location
-  resource_group_name = azurerm_resource_group.energy_application_rg.name
-  sku                 = "Standard"  # Change to your desired SKU (Standard, Basic, etc.)
-}
