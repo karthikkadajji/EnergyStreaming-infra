f918eb5cc228f0ebde8fc82b4acec2845dcfec26
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-26 16:48:22 +0200

    add datafactory

--- variable.tf
+++ variable.tf
@@ -25,7 +25,7 @@ variable "data_factory_name" {
 
 variable "storage_account_name" {
   description = "Name of the Azure Storage Account"
-  default     = "energystreamingapplication"
+  default     = "energystreamapplication"
 }
 
 variable "destination_container" {
