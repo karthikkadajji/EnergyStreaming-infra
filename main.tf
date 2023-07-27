3f6b24dbbbafdef42d8f6578f358b799947f61f5
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-27 18:23:29 +0200

    add funcapp

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_function_app" "example" {
-  name                       = "example-azure-function"
+  name                       = "example-azure-kktestdeletedfsodi"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
   app_service_plan_id        = azurerm_app_service_plan.example.id
