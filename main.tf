3e4b295548587f44198cab6e21316ada7ca46bde
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-28 13:09:42 +0200

    add funcapp

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "azurerm_app_service_plan" "example" {
   }
 }
 
-resource "azurerm_function_app" "example" {
+resource "azurerm_function_app" "fun_app" {
   name                       = "example-azure-kktestdeletedfsodi"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
@@ -105,4 +105,24 @@ resource "azurerm_function_app" "example" {
     "FUNCTIONS_WORKER_RUNTIME" = "python"
   }
 }
+resource "azurerm_key_vault" "my_key_vault" {
+  name                = "my-key-vault"
+  location            = azurerm_resource_group.energy_application_rg.location
+  resource_group_name = azurerm_resource_group.energy_application_rg.name
+
+  sku_name = "standard"
+  tenant_id = data.azurerm_client_config.current.tenant_id
+}
+
+resource "azurerm_key_vault_access_policy" "function_app_policy" {
+  key_vault_id = azurerm_key_vault.my_key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = azurerm_function_app.fun_app.identity[0].principal_id
+
+  secret_permissions = ["get"]
+}
+
+data "azurerm_client_config" "current" {}
+
 
